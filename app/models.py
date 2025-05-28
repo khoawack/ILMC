e12c7a01386db94d9dd83bea4e959e1a6128dd85
Khoa <khoacuongnguyen@gmail.com>
2025-05-28 06:15:04 -0700

    Add drop_rates table and update models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Text, Integer, Float, Boolean, ForeignKey, TIMESTAMP
-
+from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from app.database import Base
 
